6819b1f3c54c26b97ab45a92a6c07e496480c27c
Randa-Tabaza <lendaranda@gmail.com>
2023-05-20 21:54:16 +0300

    create new project

--- javaScript/MiniGame.js
+++ javaScript/MiniGame.js
@@ -19,8 +19,8 @@ function start() {
 function rollDice() {
     var dice1Value = Math.floor(Math.random() * 6) + 1;
     var dice2Value = Math.floor(Math.random() * 6) + 1;
-    dice1.setAttribute("src", "Dice images/die" + dice1Value + ".png");
-    dice2.setAttribute("src", "Dice images/die" + dice2Value + ".png");
+    dice1.setAttribute(`src`, `Dice images/die` + dice1Value + `.png`);
+    dice2.setAttribute(`src`, `Dice images/die` + dice2Value + `.png`);
     targetNumber = dice1Value + dice2Value; 
 }
 
